4b68a96a80967c0f427bfbc91ef56807b1fe3d5f
Marc <marc.doehring@icloud.com>
2025-07-18 00:56:33 +0200

    variables.scss portfolio_update

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-   currentSection = '';
+export class HeaderComponent implements OnInit {
+  currentSection = '';
   mobileMenuOpen = false;
+
+  currentLang: 'en' | 'de' = 'en';
+  languageSvg: SafeHtml = '';
+
+  constructor(
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.http.get('/header/3. Language switch.svg', { responseType: 'text' })
+      .subscribe(svg => {
+        this.languageSvg = this.sanitizer.bypassSecurityTrustHtml(svg);
+      });
+  }
+
+  toggleLanguage() {
+    this.currentLang = this.currentLang === 'en' ? 'de' : 'en';
+    console.log('Sprache ist jetzt:', this.currentLang);
+    // Hier könntest du z. B. auch eine Übersetzungsfunktion anstoßen
+  }
 
   setCurrentSection(section: string) {
     this.currentSection = section;
