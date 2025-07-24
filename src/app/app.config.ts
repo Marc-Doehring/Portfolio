0dac3d709068be6033f336d72bab2456fa660769
Marc <marc.doehring@icloud.com>
2025-07-24 16:00:48 +0200

    design_update + routing privacy policy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+// Übersetzungs-Loader
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './i18n/', '.json');
 }
@@ -14,6 +16,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(
+      // HTTP & i18n
       HttpClientModule,
       TranslateModule.forRoot({
         defaultLanguage: 'en',
@@ -22,6 +25,12 @@ export const appConfig: ApplicationConfig = {
           useFactory: createTranslateLoader,
           deps: [HttpClient],
         },
+      }),
+      // 🟢 SCROLL CONFIG HIER
+      RouterModule.forRoot([], {
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+        scrollOffset: [0, 0]
       })
     )
   ]
