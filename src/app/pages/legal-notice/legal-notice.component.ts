d65ea916820ff261cb186c2888ea502d7d4fdb60
Marc <marc.doehring@icloud.com>
2025-07-25 12:14:59 +0200

    footer responsive_update

--- src/app/pages/legal-notice/legal-notice.component.ts
+++ src/app/pages/legal-notice/legal-notice.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/header/header.component';
+
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [HeaderComponent, TranslateModule],
+  imports: [ TranslateModule],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
